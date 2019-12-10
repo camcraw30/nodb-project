29e0c1349911dc6fa674b5595b12b511fb12c5e5
camcraw30 <54972127+camcraw30@users.noreply.github.com>
2019-12-10 15:23:46 -0600

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,51 +1,48 @@
-import React, { Component } from "react";
-import axios from 'axios';
-import AddFacts from "./components/AddFacts";
-import MyFacts from "./components/MyFacts";
-// import DeleteFacts from "./components/DeleteFacts";
-import Header from './components/Header/Header';
-import Footer from './components/Footer/Footer';
+import React from 'react';
 import './App.css';
 
-
-class App extends Component {
+class App extends React.Component {
   constructor() {
     super();
+
     this.state = {
-      currentPage: "",
-      allFacts: []
+      menuStatus: 'menu-close'
     }
   }
 
-  componentDidMount() {
-    axios.get("/api/nba/facts").then(response => {
-      console.log(response.data);
-      this.setState({ allFacts: response.data })
-    }).catch(err => {
-      console.log(err);
-    })
+  handleClick = () => {
+    if (this.state.menuStatus === 'menu-close') {
+      this.setState({ menuStatus: 'menu-open' })
+    } else {
+      this.setState({ menuStatus: 'menu-close' })
+    }
   }
 
   render() {
     return (
-      <section>
-        <Header />
-        <nav className="fact-button">
-          <button className="add" onClick={() => this.setState({ currentPage: "add" })}>Add Fact</button>
-          <button className="view" onClick={() => this.setState({ currentPage: "view" })}>View Facts</button>
+      <>
+        <header>
+        <nav>
+          <h1 className='logo'>Start Bootstrap</h1>
+          <button className='menu-button' onClick={this.handleClick}>MENU</button>
+          <ul>
+            <li>Services</li>
+            <li>Portfolio</li>
+            <li>About</li>
+            <li>Team</li>
+            <li>Contact</li>
+          </ul>
         </nav>
-
-        {this.state.currentPage === "add" ? <AddFacts /> : this.state.currentPage === "view" ? ( 
-          this.state.allFacts.map((element, index) => {
-            console.log(element)
-            return (
-                <MyFacts element={element} key={index} newFactArr={this.newFactArr} />
-            )
-        })
-        ) : null}
-        <Footer />
-      </section>
-    )
+        <menu id='menu' className={this.state.menuStatus}>
+            <h1 className='menu-list'>Services</h1>
+            <h1 className='menu-list'>Portfolio</h1>
+            <h1 className='menu-list'>About</h1>
+            <h1 className='menu-list'>Team</h1>
+            <h1 className='menu-list'>Contact</h1>
+        </menu>
+        </header>
+      </>
+    );
   }
 }
 
